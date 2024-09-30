52ad3c5afcda9061fbc0fb68dddb2a2a4dde9548
seojm <myungs8562@gmail.com>
2024-09-30 16:34:56 +0900

    Feat: 랜딩 페이지 추가 기본적으로 모바일뷰, 태블릿 뷰도 지원

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import { Providers } from "./providers";
-import "../globals.css";
+import "/styles/globals.css";
+import { pretendard } from "../../styles/font";
 import SideTab from "@/components/SideTab/SideTab";
 import SideTabTest from "../../__tests__/SideTab.test";
 import AnimatedComponent from "../../__tests__/AnimatedComponent.test";
@@ -10,12 +11,15 @@ export default function RootLayout({
 	children: React.ReactNode;
 }) {
 	return (
-		<html lang="ko">
-			<body className="w-screen h-screen">
+		<html lang="ko" className={pretendard.className}>
+			<head>
+				<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+			</head>
+
+			<body className="h-screen max-w-xl mx-auto overscroll-none">
 				<Providers>
 					<div className="flex h-full w-full">
-						<SideTab />
-						{/* <SideTabTest /> */}
+						{/* <SideTab /> */}
 						<div className="w-full">{children}</div>
 					</div>
 				</Providers>
