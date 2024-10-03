3a72a80d1f2a46480162894febe9ba2223a973d8
seojm <myungs8562@gmail.com>
2024-10-03 20:36:44 +0900

    reset

--- src/containers/Answer/AnswerContainer.tsx
+++ src/containers/Answer/AnswerContainer.tsx
@@ -1,14 +1,27 @@
+"use client";
+
 import { Banner } from "@/components/Banner";
 import { CheckAnswer } from "@/components/Tables";
 import Link from "next/link";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { TestInfo } from "../../../types/Item";
+import { ProblemCount } from "../../../utils/problemCount";
 
-interface AnswerContainerProps {
-	moId: number;
-}
+const AnswerContainer = ({ testId }: { testId: number }) => {
+	const [test, setTest] = useState<TestInfo | null>();
 
+	// 페이지가 로드될 때 sessionStorage에서 선택한 아이템 데이터 가져오기
+	useEffect(() => {
+		const savedItem = sessionStorage.getItem("selectedItem");
+		if (savedItem) {
+			setTest(JSON.parse(savedItem)); // JSON 문자열을 객체로 변환하여 상태에 저장
+		}
+	}, []);
 
-const AnswerContainer = ({ moId }: AnswerContainerProps) => {
+	if (test == null) {
+		return <p>error</p>;
+	}
+
 	return (
 		<div className="p-4">
 			<Link href="/" className="inline-block">
@@ -17,7 +30,10 @@ const AnswerContainer = ({ moId }: AnswerContainerProps) => {
 			<div className="h-16 text-white px-6 my-2 text-xl font-bold bg-orange-400 rounded-lg flex items-center">
 				오답을 체크하고 답을 입력하세요!
 			</div>
-			<CheckAnswer problemNumber={40} moId={moId} />
+			<CheckAnswer
+				problemNumber={ProblemCount[test.subject]}
+				testId={test.id}
+			/>
 		</div>
 	);
 };
