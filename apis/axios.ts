ae263e694590f1f51da1e1c151d43e7868c97665
seojm <myungs8562@gmail.com>
2024-10-08 17:29:51 +0900

    Chore: baseURL hard code

--- apis/axios.ts
+++ apis/axios.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 export const api = axios.create({
-	baseURL: `${process.env.NEXT_PUBLIC_API_URL}/api/v1`,
+	// baseURL: `${process.env.NEXT_PUBLIC_API_URL}/api/v1`,
+	baseURL: "http://203.252.168.41:8081/api/v1",
 	headers: {
 		"Cache-Control": "no-store",
 		"Content-type": "application/json",
