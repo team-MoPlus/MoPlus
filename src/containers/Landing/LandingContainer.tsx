391fac60554dcc79d0c06998f309f088c45b3bfd
seojm <myungs8562@gmail.com>
2024-11-27 15:13:33 +0900

    DesignCSS: landing page div height

--- src/containers/Landing/LandingContainer.tsx
+++ src/containers/Landing/LandingContainer.tsx
@@ -3,16 +3,17 @@
 import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 const LandingContainer = () => {
 	const router = useRouter();
 
 	return (
-		<div className="flex-col justify-center items-center h-[2300px] bg-gradient-to-b from-[#ffffff] to-[#FC6C02]">
+		<div className="flex-col justify-center items-center h-[2800px] bg-gradient-to-b from-[#ffffff] to-[#FC6C02]">
 			<Image
 				src="/example_images/landing.png"
 				width={550}
-				height={550}
+				height={300}
 				alt="랜딩 페이지"
 			/>
 
