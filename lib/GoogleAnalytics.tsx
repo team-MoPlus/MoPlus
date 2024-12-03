f5e0684b278ef3ec8892ab27bdf692da2383a747
seojm <myungs8562@gmail.com>
2024-12-03 22:01:03 +0900

    test2

--- lib/GoogleAnalytics.tsx
+++ lib/GoogleAnalytics.tsx
@@ -1,21 +1,23 @@
-"use client";
-
 import Script from "next/script";
 
-export default function GoogleAnalytics() {
+export default function GoogleAnalytics({ gaId }: { gaId: string }) {
 	return (
 		<>
 			<Script
-				id="google-tag-manager"
-				strategy="afterInteractive" // 페이지 로드 후 실행
+				async
+				src={`https://www.googletagmanager.com/gtag/js
+				?id=${gaId}`}
+			/>
+			<Script
+				id="google-analytics"
 				dangerouslySetInnerHTML={{
 					__html: `
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-MT5SX4T7');
-          `,
+		window.dataLayer = window.dataLayer || [];
+		function gtag(){dataLayer.push(arguments);}
+		gtag('js', new Date());
+
+		gtag('config', '${gaId}');
+		`,
 				}}
 			/>
 		</>
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,6 @@ import "/styles/globals.css";
 import { pretendard } from "../../styles/font";
 import { Metadata } from "next";
 import GoogleAnalytics from "../../lib/GoogleAnalytics";
-import Script from "next/script";
 
 export const metadata: Metadata = {
 	title: "모플, 쉽고 빠른 모의고사 분석",
@@ -21,19 +20,9 @@ export default function RootLayout({
 	return (
 		<html lang="ko" className={pretendard.className}>
 			<head>
-				<Script
-					id="google-tag-manager"
-					strategy="afterInteractive" // 페이지 로드 후 실행
-					dangerouslySetInnerHTML={{
-						__html: `
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-MT5SX4T7');
-          `,
-					}}
-				/>
+				{process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS ? (
+					<GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS} />
+				) : null}
 				<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 			</head>
 
