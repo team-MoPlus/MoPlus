2b51cbf126e0bf290424d37983b0cde333c4c827
seojm <myungs8562@gmail.com>
2024-12-03 21:59:19 +0900

    no component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "/styles/globals.css";
 import { pretendard } from "../../styles/font";
 import { Metadata } from "next";
 import GoogleAnalytics from "../../lib/GoogleAnalytics";
+import Script from "next/script";
 
 export const metadata: Metadata = {
 	title: "모플, 쉽고 빠른 모의고사 분석",
@@ -20,7 +21,19 @@ export default function RootLayout({
 	return (
 		<html lang="ko" className={pretendard.className}>
 			<head>
-				<GoogleAnalytics />
+				<Script
+					id="google-tag-manager"
+					strategy="afterInteractive" // 페이지 로드 후 실행
+					dangerouslySetInnerHTML={{
+						__html: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-MT5SX4T7');
+          `,
+					}}
+				/>
 				<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 			</head>
 
@@ -30,14 +43,8 @@ export default function RootLayout({
 						src="https://www.googletagmanager.com/ns.html?id=GTM-MT5SX4T7"
 						height="0"
 						width="0"
-					>
-						<style jsx>{`
-							style= {
-								display: none;
-								visibility: hidden;
-							}
-						`}</style>
-					</iframe>
+						className="hidden invisible"
+					></iframe>
 				</noscript>
 				<Providers>
 					<div className="flex h-full w-full">
